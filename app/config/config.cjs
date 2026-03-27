b24a181a22b5593cdef306566eaecd3c3cf83010
Morner01 <van.maltsev2005@yandex.ru>
2026-03-27 19:32:56 +0300

    change config & docker-compose

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -10,6 +10,15 @@ module.exports = {
     dialect: 'postgres',
     logging: console.log
   },
+  test: {
+    username: process.env.TEST_DB_USER || 'postgres',
+    password: process.env.TEST_DB_PASSWORD || 'postgres',
+    database: process.env.TEST_DB_NAME || 'blog_test',
+    host: process.env.TEST_DB_HOST || 'localhost',
+    port: process.env.TEST_DB_PORT || 5432,
+    dialect: 'postgres',
+    logging: false
+  },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
